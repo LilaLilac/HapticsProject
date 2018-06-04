fecec5445f6708051a961f431e178505b34e0270
RiraMiyata <junehydrangea626@gmail.com>
2018-06-04 14:15:37 +0900

    no message

---
+++ HapticsProject1/Assets/Scripts/NotesTimingMaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesTimingMaker : MonoBehaviour
+{
+
+    private AudioSource _audioSource;
+
+    public GameObject startButton;
+
+    void Start()
+    {
+        _audioSource = GameObject.Find("GameMusic").GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartMusic()
+    {
+        startButton.SetActive(false);
+        _audioSource.Play();
+    }
+}
