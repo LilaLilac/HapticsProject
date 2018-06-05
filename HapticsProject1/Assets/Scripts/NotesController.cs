cbc6aa37ad5296131dff067e9dd30c47824c922a
RiraMiyata <junehydrangea626@gmail.com>
2018-06-05 15:24:34 +0900

    ノーツが動くように TestSceneを作成、そこで検証中

---
+++ HapticsProject1/Assets/Scripts/NotesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesController : MonoBehaviour {
+
+    public float speed = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        speed*= 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(-this.speed, 0, 0);
+    }
+}
