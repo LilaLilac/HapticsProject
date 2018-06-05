aff9eed436c198af296dcbb0bbb4d282053ece07
RiraMiyata <junehydrangea626@gmail.com>
2018-06-05 16:17:29 +0900

    ノーツを任意の場所へ移動

--- HapticsProject1/Assets/Scripts/NotesController.cs
+++ HapticsProject1/Assets/Scripts/NotesController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class NotesController : MonoBehaviour {
 
-    public float speed = 0;
-
-
+    //public float speed = 0;
+    Vector3 endposision =new Vector3(-3, 3, 0);
+    
 	// Use this for initialization
 	void Start () {
-        speed*= 0.1f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        this.transform.Translate(-this.speed, 0, 0);
+        //speed*= 0.1f;
+        //this.transform.Translate(-this.speed, 0, 0);
+        iTween.MoveTo(this.gameObject, iTween.Hash("position", endposision, "time", 2, "easeType", "linear","delay", 1.0f));
+
+        iTween.MoveTo(this.gameObject, iTween.Hash("position", endposision+new Vector3(-1, -1, 0), "time", 0.5, "easeType", "linear"));
     }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+    
+
 }
