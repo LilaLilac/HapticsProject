b8dc313f6ef7513a6c807235689dff0840e64f89
honoka <mukai-honoka377@g.ecc.u-tokyo.ac.jp>
2018-06-05 17:39:25 +0900

    UIにテキストと画像を設置。Timer実装。スコアとTimerの連動ができない

---
+++ HapticsProject1/Assets/Scripts/ScoreDirector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreDirector : MonoBehaviour {
+    GameObject gage;
+    // Use this for initialization
+    void Start () {
+       this.gage = GameObject.Find("gage");
+    }
+	
+	public void DecreaseHp() {
+        this.gage.GetComponent<Image>().fillAmount *= 0.7f;
+	}
+}
