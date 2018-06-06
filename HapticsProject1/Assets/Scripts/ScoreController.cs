90596a5dea7674c203189db2a92e0bd68ce6780b
honoka <mukai-honoka377@g.ecc.u-tokyo.ac.jp>
2018-06-06 16:09:58 +0900

    点数表示を追加した。押している間goodを出すのが微妙にうまくいってない...？

--- HapticsProject1/Assets/Scripts/ScoreController.cs
+++ HapticsProject1/Assets/Scripts/ScoreController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
-{ public float now = 0f;
-    public float start = 1.0f;
-    public float end = 5.0f;
+{
+    public float now = 0f;
+    public float start = 1.0f;//入力開始時刻
+    public float end = 5.0f;//入力終了時刻
+    int score = 0;//スコア
     // Use this for initialization
     void Start()
     {
@@ -15,9 +18,9 @@ public class ScoreController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        now += Time.deltaTime;
+        now += Time.deltaTime;//現在の時刻取得
         //Debug.Log(now);
-        if(now >= start && now <= end)
+        if (now >= start && now <= end)
         {
             ScoreI();
         }
@@ -26,27 +29,33 @@ public class ScoreController : MonoBehaviour
             ScoreD();
         }
     }
-    void ScoreI()
+    void ScoreI()//入力すべきタイミングでの判定
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Good");
-        }
-        else
-        {
-            Debug.Log("Bad");
-        }
-    }
-    void ScoreD()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.Log("Bad");
-        }
-        else
-        {
-            Debug.Log("Newtral");
-        }
-    }
 
+            GetComponent<Text>().text="Good";
+
+        }
+        else
+        {
+            GetComponent<Text>().text="Bad";
+            if (score > 0)
+            {
+                score -= 1;
+            }
+        }
+    }
+    void ScoreD()//入力してはならない時の判定
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Text>().text ="Bad";
+        }
+        else
+        {
+            GetComponent<Text>().text = "";
+        }
+    }
 }
+
