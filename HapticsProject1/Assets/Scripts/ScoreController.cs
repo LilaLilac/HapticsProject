ee785c81f3df52b57e97dc9a58d413c2f406977a
honoka <mukai-honoka377@g.ecc.u-tokyo.ac.jp>
2018-06-06 16:18:00 +0900

    点数の変化速度を調整。

--- HapticsProject1/Assets/Scripts/ScoreController.cs
+++ HapticsProject1/Assets/Scripts/ScoreController.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
+    public float score = 0f;
     public float now = 0f;
-    public float start = 1.0f;//入力開始時刻
-    public float end = 5.0f;//入力終了時刻
-    int score = 0;//スコア
+    private float start = 10.0f;//入力開始時刻
+    private float end = 16.0f;//入力終了時刻
     // Use this for initialization
     void Start()
     {
@@ -19,7 +19,7 @@ public class ScoreController : MonoBehaviour
     void Update()
     {
         now += Time.deltaTime;//現在の時刻取得
-        //Debug.Log(now);
+        GameObject.Find("Timer").GetComponent<Text>().text = now.ToString("F2"); //時刻ひょうじ
         if (now >= start && now <= end)
         {
             ScoreI();
@@ -28,33 +28,49 @@ public class ScoreController : MonoBehaviour
         {
             ScoreD();
         }
+
     }
-    void ScoreI()//入力すべきタイミングでの判定
+    void ScoreI()//入力すべきタイミングでの判定と点数表示
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            score += 0.05f;
 
-            GetComponent<Text>().text="Good";
+            GameObject.Find("Comment").GetComponent<Text>().text="Good";
+            GameObject.Find("ScoreText").GetComponent<Text>().text= score.ToString("F0");
 
         }
         else
         {
-            GetComponent<Text>().text="Bad";
-            if (score > 0)
+            if(score > 0)
             {
-                score -= 1;
+                score -= 0.05f;
+                GameObject.Find("Comment").GetComponent<Text>().text = "Bad";
+                GameObject.Find("ScoreText").GetComponent<Text>().text = score.ToString("F0");
+
             }
+            else
+            {
+                GameObject.Find("Comment").GetComponent<Text>().text = "Bad";
+                GameObject.Find("ScoreText").GetComponent<Text>().text = score.ToString("F0");
+            }
+            
         }
     }
-    void ScoreD()//入力してはならない時の判定
+    void ScoreD()//入力してはならない時の判定と点数表示
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            GetComponent<Text>().text ="Bad";
+            
+            GameObject.Find("Comment").GetComponent<Text>().text ="Bad";
+            GameObject.Find("ScoreText").GetComponent<Text>().text = score.ToString("F0");
+
         }
         else
         {
-            GetComponent<Text>().text = "";
+            GameObject.Find("Comment").GetComponent<Text>().text = "";
+            GameObject.Find("ScoreText").GetComponent<Text>().text = score.ToString("F0");
+
         }
     }
 }
